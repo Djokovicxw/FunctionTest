2a1c7998dd92f8f8b3a136b5b8742c8651fdedbf
Djokovicxw <38768687@qq.com>
2019-01-15 18:00:11 +0800

    Unittest

--- func_test.py
+++ func_test.py
@@ -11,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_restrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('TO-DO', self.browser.title)
         self.fail('Finish the test!')
 
 if __name__ =='__main__':
