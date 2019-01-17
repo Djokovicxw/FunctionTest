7150feeae5def7781616be21959d7e7228d11f1b
Djokovicxw <38768687@qq.com>
2019-01-17 14:36:10 +0800

    Checks if we can input a to-do item

--- func_test.py
+++ func_test.py
@@ -14,9 +14,14 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_restrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('TO-DO', self.browser.title)
-        header_text = self.browser.find_element_by_id('id_new_item')
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        inputbox  = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
                 'Enter a to-do item')
+        
         inputbox.send_keys('Buy a peacok feathers')
         inputbox.send_keys(Keys.ENTER)
 
