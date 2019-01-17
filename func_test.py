e8954ca0d7c96f643619affb06b0d51ecc0b0516
Djokovicxw <38768687@qq.com>
2019-01-17 16:01:04 +0800

    Update

--- func_test.py
+++ func_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -22,15 +23,15 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                 'Enter a to-do item')
         
-        inputbox.send_keys('Buy a peacok feathers')
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
-                )
+                any(row.text == '1: Buy peacock feathers' for row in rows), 
+                    'its text was: %s' % (table.text))
 
         self.fail('Finish the test!')
 
